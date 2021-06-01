7f548e3b834f4c1a88e97c2b62d1d494fa6b3909
htchepannou <htchepannou@expedia.com>
2021-06-01 16:03:10 -0400

    update tracking

--- src/main/kotlin/com/wutsi/tracking/TrackingApi.kt
+++ src/main/kotlin/com/wutsi/tracking/TrackingApi.kt
@@ -2,9 +2,11 @@ package com.wutsi.tracking
 
 import com.wutsi.tracking.dto.PushTrackRequest
 import com.wutsi.tracking.dto.PushTrackResponse
+import feign.Headers
 import feign.RequestLine
 
 public interface TrackingApi {
   @RequestLine("POST /v1/tracks")
+  @Headers("Content-Type: application/json")
   public fun push(request: PushTrackRequest): PushTrackResponse
 }
--- src/main/kotlin/com/wutsi/tracking/TrackingApiBuilder.kt
+++ src/main/kotlin/com/wutsi/tracking/TrackingApiBuilder.kt
@@ -1,14 +1,18 @@
 package com.wutsi.tracking
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import feign.Request
 import feign.RequestInterceptor
+import feign.Retryer
 import kotlin.collections.List
 
 public class TrackingApiBuilder {
   public fun build(
     env: Environment,
     mapper: ObjectMapper,
-    interceptors: List<RequestInterceptor> = emptyList()
+    interceptors: List<RequestInterceptor> = emptyList(),
+    options: Request.Options = Request.Options(),
+    retryer: Retryer = Retryer.Default()
   ) = feign.Feign.builder()
     .client(feign.okhttp.OkHttpClient())
     .encoder(feign.jackson.JacksonEncoder(mapper))
@@ -16,5 +20,7 @@ public class TrackingApiBuilder {
     .logger(feign.slf4j.Slf4jLogger(TrackingApi::class.java))
     .logLevel(feign.Logger.Level.BASIC)
     .requestInterceptors(interceptors)
+    .options(options)
+    .retryer(retryer)
     .target(TrackingApi::class.java, env.url)
 }
